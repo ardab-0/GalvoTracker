8a253e394200c4398b84aee7fe94645c7f7adc8c
ardabuglagil <ardabuglagil@gmail.com>
2023-05-31 11:26:01 +0200

    added buttons for fine adjustment

--- mirror_gui.py
+++ mirror_gui.py
@@ -21,6 +21,45 @@ def update_mirror(event):
     print("frame time ", time.time() - start)
 
 
+# def update_mirror(event):
+#     start = time.time()
+#     # time.sleep(0.030)
+#     y_t = np.array([w2.get()])
+#     x_t = np.array([w1.get()])
+    
+#     si_0.SetXY(y_t[0]/100)        
+#     si_1.SetXY(x_t[0]/100) 
+
+#     print("frame time ", time.time() - start)
+
+def increaseX():    
+    w1.set(w1.get()+1)
+    
+
+def decreaseX():    
+    w1.set(w1.get()-1)
+    
+
+
+
+def increaseY():    
+    w2.set(w2.get()+1)
+    
+
+def decreaseY():   
+    w2.set(w2.get()-1)
+    
+
+
+
+def increaseZ():
+    w3.set(w3.get()+1)
+
+   
+
+def decreaseZ():
+    w3.set(w3.get()-1)
+    
 
 # initialize mirrors
 mre2 = optoMDC.connect()
@@ -46,17 +85,27 @@ si_1 = mre2.Mirror.Channel_1.StaticInput
  
 
 master = tk.Tk()
-w1 = tk.Scale(master, from_=-50, to=50, tickinterval=1, command=update_mirror)
+w1 = tk.Scale(master, from_=-500, to=500, tickinterval=1, command=update_mirror)
 w1.set(0)
 w1.pack()
-w2 = tk.Scale(master, from_=-50, to=50,tickinterval=1, command=update_mirror)
+tk.Button(master, text='Increase X', command=increaseX).pack()
+tk.Button(master, text='Decrease X', command=decreaseX).pack()
+
+
+
+w2 = tk.Scale(master, from_=-500, to=500,tickinterval=1, command=update_mirror)
 w2.set(0)
 w2.pack()
-w3 = tk.Scale(master, from_=20, to=1000,tickinterval=1,  command=update_mirror)
-w3.set(300)
-w3.pack()
+tk.Button(master, text='Increase Y', command=increaseY).pack()
+tk.Button(master, text='Decrease Y', command=decreaseY).pack()
 
-tk.Button(master, text='Show', command=show_values).pack()
+
+w3 = tk.Scale(master, from_=20, to=1000,tickinterval=1,  command=update_mirror)
+w3.set(540)
+w3.pack()
+tk.Button(master, text='Increase Z', command=increaseZ).pack()
+tk.Button(master, text='Decrease Z', command=decreaseZ).pack()
+
 
 master.mainloop()
 
