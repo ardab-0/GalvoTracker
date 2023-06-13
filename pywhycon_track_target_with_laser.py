0c12984c6f9d863fc47fcb8a991f14e22b2f4487
ardabuglagil <arda.buglagil@fau.de>
2023-06-13 13:34:56 +0200

    pywhycon implementation of tracker

---
+++ pywhycon_track_target_with_laser.py
@@ -0,0 +1,138 @@
+import cv2
+
+import pykinect_azure as pykinect
+from pykinect_azure import K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH, k4a_float2_t
+import numpy as np
+from image_processing.circle_detector import detect_circle_position
+import optoMDC
+from mirror.coordinate_transformation import CoordinateTransform
+import pickle
+import time
+from pywhycon.build.Release.circle_detector_module import *
+
+
+# Constants 
+
+d = 0
+mirror_rotation_deg = 45
+
+save_path = "calibration_parameters"
+
+
+with open('{}/parameters.pkl'.format(save_path), 'rb') as f:
+    loaded_dict = pickle.load(f)
+    R = loaded_dict["R"]
+    t = loaded_dict["t"]
+
+
+
+
+# initialize mirrors
+mre2 = optoMDC.connect()
+mre2.reset()
+
+# Set up mirror in closed loop control mode(XY)
+ch_0 = mre2.Mirror.Channel_0
+ch_0.StaticInput.SetAsInput()                       # (1) here we tell the Manager that we will use a static input
+ch_0.SetControlMode(optoMDC.Units.XY)           
+ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+si_0 = mre2.Mirror.Channel_0.StaticInput
+
+
+ch_1 = mre2.Mirror.Channel_1
+ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Manager that we will use a static input
+ch_1.SetControlMode(optoMDC.Units.XY)           
+ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+si_1 = mre2.Mirror.Channel_1.StaticInput
+
+
+# Initialize the library, if the library is not found, add the library path as argument
+pykinect.initialize_libraries()
+
+# Modify camera configuration
+device_config = pykinect.default_configuration
+device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
+device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_720P
+device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
+# print(device_config)
+
+# Start device
+device = pykinect.start_device(config=device_config)
+
+cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
+
+prevCircle = CircleClass()
+circle_detector = CircleDetectorClass(1280, 720) # K4A_COLOR_RESOLUTION_720P
+
+
+while True:
+    start = time.time()
+    # Get capture
+    capture = device.update()
+
+    # Get the color image from the capture
+    ret_color, color_image = capture.get_color_image()
+
+    # Get the colored depth
+    ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
+
+    if not ret_color or not ret_depth:
+        continue  
+
+    new_circle = circle_detector.detect_np(color_image, prevCircle)
+    if new_circle is  None:
+        print("Target is not detected")
+        continue
+    prevCircle = new_circle
+
+    color_image = cv2.circle(color_image, (int(new_circle.x), int(new_circle.y)), radius=10, color=(0, 255, 0), thickness=2)
+   
+    
+    
+    # Show detected target position
+    cv2.imshow('Laser Detector',color_image)
+
+
+    pix_x = int(new_circle.x)
+    pix_y = int(new_circle.y)
+    rgb_depth = transformed_depth_image[pix_y, pix_x]
+
+    pixels = k4a_float2_t((pix_x, pix_y))
+
+    pos3d_color = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_COLOR)
+    # pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+    # print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
+
+    camera_coordinates = np.array([pos3d_color.xyz.x, pos3d_color.xyz.y, pos3d_color.xyz.z]).reshape((3, 1))
+
+    # rotate and translate
+
+    
+    
+
+    # camera_coordinates_in_laser_coordinates = camera_coordinates + np.array([130, 50, 70]).reshape((3, 1))  # should find parameters automatically
+
+    camera_coordinates_in_laser_coordinates =  R @ camera_coordinates + t
+  
+
+    coordinate_transform = CoordinateTransform(d=d, D=camera_coordinates_in_laser_coordinates[2], rotation_degree=mirror_rotation_deg)
+
+
+
+    y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1], camera_coordinates_in_laser_coordinates[0]) # order is changed in order to change x and y axis
+
+    del coordinate_transform
+    
+    if(len(y_m) > 0 and len(x_m) > 0):
+        si_0.SetXY(y_m[0])        
+        si_1.SetXY(x_m[0]) 
+
+
+    print("fps: ", 1 / (time.time() - start))
+    # Press q key to stop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+
+mre2.disconnect()
+print("done")
