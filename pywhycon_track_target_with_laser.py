e9e1f644c6c020f7bb841da82857b559e8f42995
ardabuglagil <ardabuglagil@gmail.com>
2023-06-15 13:06:27 +0200

    add text description

--- pywhycon_track_target_with_laser.py
+++ pywhycon_track_target_with_laser.py
@@ -8,7 +8,8 @@ import optoMDC
 from mirror.coordinate_transformation import CoordinateTransform
 import pickle
 import time
-from pywhycon.build.Release.circle_detector_module import *
+import os
+from circle_detector_library.circle_detector_module import *
 
 
 # Constants 
@@ -60,6 +61,7 @@ device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
 device = pykinect.start_device(config=device_config)
 
 cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 prevCircle = CircleClass()
 circle_detector = CircleDetectorClass(1280, 720) # K4A_COLOR_RESOLUTION_720P
@@ -75,21 +77,22 @@ while True:
 
     # Get the colored depth
     ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
-
+    
     if not ret_color or not ret_depth:
         continue  
+    
 
+    color_image_3channel = color_image[:, :, :3]
     # returns 0, 0 if target is not detected
-    new_circle = circle_detector.detect_np(color_image, prevCircle)
+    
+    new_circle = circle_detector.detect_np(color_image_3channel, prevCircle)
     
     prevCircle = new_circle
 
-    color_image = cv2.circle(color_image, (int(new_circle.x), int(new_circle.y)), radius=10, color=(0, 255, 0), thickness=2)
    
     
     
-    # Show detected target position
-    cv2.imshow('Laser Detector',color_image)
+    
 
 
     pix_x = int(new_circle.x)
@@ -120,14 +123,22 @@ while True:
 
     y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1], camera_coordinates_in_laser_coordinates[0]) # order is changed in order to change x and y axis
 
-    del coordinate_transform
     
     if(len(y_m) > 0 and len(x_m) > 0):
         si_0.SetXY(y_m[0])        
         si_1.SetXY(x_m[0]) 
 
+    end = time.time()
+    print("elapsed time: ", (end - start))    
+    cv2.putText(color_image, f"fps: {1 / (end - start)}", (10, 20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
-    print("fps: ", 1 / (time.time() - start))
+    cv2.putText(color_image, f"Target Coordinates w.r.t. mirror center:", (10, 40), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(color_image, f"X: {camera_coordinates_in_laser_coordinates[0]}", (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(color_image, f"Y: {camera_coordinates_in_laser_coordinates[1]}", (10, 80), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.putText(color_image, f"Z: {camera_coordinates_in_laser_coordinates[2]}", (10, 100), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+    color_image = cv2.circle(color_image, (int(new_circle.x), int(new_circle.y)), radius=10, color=(0, 255, 0), thickness=2)
+    # Show detected target position
+    cv2.imshow('Laser Detector',color_image)
     # Press q key to stop
     if cv2.waitKey(1) == ord('q'):
         break
