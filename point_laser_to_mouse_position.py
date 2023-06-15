539cfcf804c0a912cad9d953e0af96d9319b92e2
ardabuglagil <ardabuglagil@gmail.com>
2023-06-15 13:24:41 +0200

    add main function

--- point_laser_to_mouse_position.py
+++ point_laser_to_mouse_position.py
@@ -21,8 +21,7 @@ d = 0
 mirror_rotation_deg = 45
 save_path = "calibration_parameters"
 # target coordinate offset (mm)
-y_offset = 0
-z_offset_factor = 1
+
 
 with open('{}/parameters.pkl'.format(save_path), 'rb') as f:
     loaded_dict = pickle.load(f)
@@ -41,126 +40,131 @@ def onMousemove(event, x, y, flags, param):
 
 
 
-             
+def main():            
 
-# initialize mirrors
-mre2 = optoMDC.connect()
-mre2.reset()
+    # initialize mirrors
+    mre2 = optoMDC.connect()
+    mre2.reset()
 
-# Set up mirror in closed loop control mode(XY)
-ch_0 = mre2.Mirror.Channel_0
-ch_0.StaticInput.SetAsInput()                       # (1) here we tell the Manager that we will use a static input
-ch_0.SetControlMode(optoMDC.Units.XY)           
-ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
-si_0 = mre2.Mirror.Channel_0.StaticInput
-
-
-ch_1 = mre2.Mirror.Channel_1
-ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Manager that we will use a static input
-ch_1.SetControlMode(optoMDC.Units.XY)           
-ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
-si_1 = mre2.Mirror.Channel_1.StaticInput
+    # Set up mirror in closed loop control mode(XY)
+    ch_0 = mre2.Mirror.Channel_0
+    ch_0.StaticInput.SetAsInput()                       # (1) here we tell the Manager that we will use a static input
+    ch_0.SetControlMode(optoMDC.Units.XY)           
+    ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+    si_0 = mre2.Mirror.Channel_0.StaticInput
 
 
-# Initialize the library, if the library is not found, add the library path as argument
-pykinect.initialize_libraries()
-
-# Modify camera configuration
-device_config = pykinect.default_configuration
-device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
-# device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1080P
-device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
-# print(device_config)
-
-# Start device
-device = pykinect.start_device(config=device_config)
-
-transformation = Transformation(device.get_calibration(device_config.depth_mode, device_config.color_resolution))
+    ch_1 = mre2.Mirror.Channel_1
+    ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Manager that we will use a static input
+    ch_1.SetControlMode(optoMDC.Units.XY)           
+    ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+    si_1 = mre2.Mirror.Channel_1.StaticInput
 
 
-cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
-cv2.setMouseCallback('Laser Detector', onMousemove)
+    # Initialize the library, if the library is not found, add the library path as argument
+    pykinect.initialize_libraries()
+
+    # Modify camera configuration
+    device_config = pykinect.default_configuration
+    device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
+    # device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1080P
+    device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
+    # print(device_config)
+
+    # Start device
+    device = pykinect.start_device(config=device_config)
+
+    transformation = Transformation(device.get_calibration(device_config.depth_mode, device_config.color_resolution))
 
 
-while True:
-    start = time.time()
-    # Get capture
-    capture = device.update()
-
-    # Get the color image from the capture
-    ret_color, color_image = capture.get_color_image()
-
-    # Get the colored depth
-    ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
-    
-
-    if not ret_color or not ret_depth:
-        continue  
-
-    
-    pix_x = mouse_x
-    pix_y = mouse_y
-    rgb_depth = transformed_depth_image[pix_y, pix_x]
-
-    pixels = k4a_float2_t((pix_x, pix_y))
+    cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback('Laser Detector', onMousemove)
 
 
+    while True:
+        start = time.time()
+        # Get capture
+        capture = device.update()
 
-    pos3d_color = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_COLOR)
-    # pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
-    # print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
+        # Get the color image from the capture
+        ret_color, color_image = capture.get_color_image()
 
-    camera_coordinates = np.array([pos3d_color.xyz.x, pos3d_color.xyz.y, pos3d_color.xyz.z]).reshape((3, 1))
+        # Get the colored depth
+        ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
+        
 
-   
-    # Test point cloud 
-    # point_cloud_image = transformation.depth_image_to_point_cloud(transformed_depth_image, K4A_CALIBRATION_TYPE_COLOR)
-    
+        if not ret_color or not ret_depth:
+            continue  
+
+        
+        pix_x = mouse_x
+        pix_y = mouse_y
+        rgb_depth = transformed_depth_image[pix_y, pix_x]
+
+        pixels = k4a_float2_t((pix_x, pix_y))
+
+
+
+        pos3d_color = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_COLOR)
+        # pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+        # print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
+
+        camera_coordinates = np.array([pos3d_color.xyz.x, pos3d_color.xyz.y, pos3d_color.xyz.z]).reshape((3, 1))
 
     
-    # rotate and translate
+        # Test point cloud 
+        # point_cloud_image = transformation.depth_image_to_point_cloud(transformed_depth_image, K4A_CALIBRATION_TYPE_COLOR)
+        
 
-    camera_coordinates_in_laser_coordinates =  R @ camera_coordinates + t
+        
+        # rotate and translate
 
-
-    # camera_coordinates_in_laser_coordinates = np.floor(camera_coordinates_in_laser_coordinates)
-
-    # print("camera_coordinates", camera_coordinates)
-
-    # print("camera_coordinates_in_laser_coordinates", camera_coordinates_in_laser_coordinates)
+        camera_coordinates_in_laser_coordinates =  R @ camera_coordinates + t
 
 
-    coordinate_transform = CoordinateTransform(d=d, D=camera_coordinates_in_laser_coordinates[2], rotation_degree=mirror_rotation_deg)
+        # camera_coordinates_in_laser_coordinates = np.floor(camera_coordinates_in_laser_coordinates)
+
+        # print("camera_coordinates", camera_coordinates)
+
+        # print("camera_coordinates_in_laser_coordinates", camera_coordinates_in_laser_coordinates)
+
+
+        coordinate_transform = CoordinateTransform(d=d, D=camera_coordinates_in_laser_coordinates[2], rotation_degree=mirror_rotation_deg)
 
 
 
-    y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1]+y_offset, camera_coordinates_in_laser_coordinates[0]*z_offset_factor) # order is changed in order to change x and y axis
+        y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1]+y_offset, camera_coordinates_in_laser_coordinates[0]*z_offset_factor) # order is changed in order to change x and y axis
 
-    
-    
-    if(len(y_m) > 0 and len(x_m) > 0):
-        si_0.SetXY(y_m[0])        
-        si_1.SetXY(x_m[0]) 
-
-
-    
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(color_image, f"fps: {1 / (time.time() - start)}", (10, 20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-
-    cv2.putText(color_image, f"Target Coordinates w.r.t. mirror center:", (10, 40), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"X: {camera_coordinates_in_laser_coordinates[0]}", (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"Y: {camera_coordinates_in_laser_coordinates[1]}", (10, 80), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"Z: {camera_coordinates_in_laser_coordinates[2]}", (10, 100), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        
+        
+        if(len(y_m) > 0 and len(x_m) > 0):
+            si_0.SetXY(y_m[0])        
+            si_1.SetXY(x_m[0]) 
 
 
-      
-    # cv2.circle(color_image, center=(mouse_x, mouse_y), radius=10, color=(0, 255, 0), thickness=2)
-    # Show detected target position
-    cv2.imshow('Laser Detector',color_image)
-    # Press q key to stop
-    if cv2.waitKey(1) == ord('q'):
-        break
+        
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(color_image, f"fps: {1 / (time.time() - start)}", (10, 20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+
+        cv2.putText(color_image, f"Target Coordinates w.r.t. mirror center:", (10, 40), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"X: {camera_coordinates_in_laser_coordinates[0]}", (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"Y: {camera_coordinates_in_laser_coordinates[1]}", (10, 80), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"Z: {camera_coordinates_in_laser_coordinates[2]}", (10, 100), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
 
-mre2.disconnect()
-print("done")
+        
+        # cv2.circle(color_image, center=(mouse_x, mouse_y), radius=10, color=(0, 255, 0), thickness=2)
+        # Show detected target position
+        cv2.imshow('Laser Detector',color_image)
+        # Press q key to stop
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+    mre2.disconnect()
+    print("done")
+
+
+
+if __name__ == "__main__":
+     main()
--- pywhycon_track_target_with_laser.py
+++ pywhycon_track_target_with_laser.py
@@ -27,122 +27,114 @@ with open('{}/parameters.pkl'.format(save_path), 'rb') as f:
 
 
 
+def main():
+    # initialize mirrors
+    mre2 = optoMDC.connect()
+    mre2.reset()
 
-# initialize mirrors
-mre2 = optoMDC.connect()
-mre2.reset()
-
-# Set up mirror in closed loop control mode(XY)
-ch_0 = mre2.Mirror.Channel_0
-ch_0.StaticInput.SetAsInput()                       # (1) here we tell the Manager that we will use a static input
-ch_0.SetControlMode(optoMDC.Units.XY)           
-ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
-si_0 = mre2.Mirror.Channel_0.StaticInput
-
-
-ch_1 = mre2.Mirror.Channel_1
-ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Manager that we will use a static input
-ch_1.SetControlMode(optoMDC.Units.XY)           
-ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
-si_1 = mre2.Mirror.Channel_1.StaticInput
+    # Set up mirror in closed loop control mode(XY)
+    ch_0 = mre2.Mirror.Channel_0
+    ch_0.StaticInput.SetAsInput()                       # (1) here we tell the Manager that we will use a static input
+    ch_0.SetControlMode(optoMDC.Units.XY)           
+    ch_0.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+    si_0 = mre2.Mirror.Channel_0.StaticInput
 
 
-# Initialize the library, if the library is not found, add the library path as argument
-pykinect.initialize_libraries()
-
-# Modify camera configuration
-device_config = pykinect.default_configuration
-device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
-device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_720P
-device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
-# print(device_config)
-
-# Start device
-device = pykinect.start_device(config=device_config)
-
-cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-prevCircle = CircleClass()
-circle_detector = CircleDetectorClass(1280, 720) # K4A_COLOR_RESOLUTION_720P
+    ch_1 = mre2.Mirror.Channel_1
+    ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Manager that we will use a static input
+    ch_1.SetControlMode(optoMDC.Units.XY)           
+    ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
+    si_1 = mre2.Mirror.Channel_1.StaticInput
 
 
-while True:
-    start = time.time()
-    # Get capture
-    capture = device.update()
+    # Initialize the library, if the library is not found, add the library path as argument
+    pykinect.initialize_libraries()
 
-    # Get the color image from the capture
-    ret_color, color_image = capture.get_color_image()
+    # Modify camera configuration
+    device_config = pykinect.default_configuration
+    device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
+    device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_720P
+    device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
+    # print(device_config)
 
-    # Get the colored depth
-    ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
-    
-    if not ret_color or not ret_depth:
-        continue  
-    
+    # Start device
+    device = pykinect.start_device(config=device_config)
 
-    color_image_3channel = color_image[:, :, :3]
-    # returns 0, 0 if target is not detected
-    
-    new_circle = circle_detector.detect_np(color_image_3channel, prevCircle)
-    
-    prevCircle = new_circle
-
-   
-    
-    
-    
+    cv2.namedWindow('Laser Detector',cv2.WINDOW_NORMAL)
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-    pix_x = int(new_circle.x)
-    pix_y = int(new_circle.y)
-    rgb_depth = transformed_depth_image[pix_y, pix_x]
+    # gives undefined warning but works
+    prevCircle = CircleClass()
+    circle_detector = CircleDetectorClass(1280, 720) # K4A_COLOR_RESOLUTION_720P
 
-    pixels = k4a_float2_t((pix_x, pix_y))
 
-    pos3d_color = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_COLOR)
-    # pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
-    # print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
+    while True:
+        start = time.time()
+        # Get capture
+        capture = device.update()
 
-    camera_coordinates = np.array([pos3d_color.xyz.x, pos3d_color.xyz.y, pos3d_color.xyz.z]).reshape((3, 1))
+        # Get the color image from the capture
+        ret_color, color_image = capture.get_color_image()
+        
+        # Get the colored depth
+        ret_depth, transformed_depth_image = capture.get_transformed_depth_image()
+        
+        if not ret_color or not ret_depth:
+            continue  
+        
 
-    # rotate and translate
+        color_image_3channel = color_image[:, :, :3]
+        # returns 0, 0 if target is not detected
+        
+        new_circle = circle_detector.detect_np(color_image_3channel, prevCircle)    
+        prevCircle = new_circle
 
     
+        
+        pix_x = int(new_circle.x)
+        pix_y = int(new_circle.y)
+        rgb_depth = transformed_depth_image[pix_y, pix_x]
+
+        pixels = k4a_float2_t((pix_x, pix_y))
+
+        pos3d_color = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_COLOR)
+        # pos3d_depth = device.calibration.convert_2d_to_3d(pixels, rgb_depth, K4A_CALIBRATION_TYPE_COLOR, K4A_CALIBRATION_TYPE_DEPTH)
+        # print(f"RGB depth: {rgb_depth}, RGB pos3D: {pos3d_color}, Depth pos3D: {pos3d_depth}")
+
+        camera_coordinates = np.array([pos3d_color.xyz.x, pos3d_color.xyz.y, pos3d_color.xyz.z]).reshape((3, 1))
+
+        # rotate and translate  
+        camera_coordinates_in_laser_coordinates =  R @ camera_coordinates + t
     
 
-    # camera_coordinates_in_laser_coordinates = camera_coordinates + np.array([130, 50, 70]).reshape((3, 1))  # should find parameters automatically
+        coordinate_transform = CoordinateTransform(d=d, D=camera_coordinates_in_laser_coordinates[2], rotation_degree=mirror_rotation_deg)
+        y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1], camera_coordinates_in_laser_coordinates[0]) # order is changed in order to change x and y axis
 
-    camera_coordinates_in_laser_coordinates =  R @ camera_coordinates + t
-  
+        
+        if(len(y_m) > 0 and len(x_m) > 0):
+            si_0.SetXY(y_m[0])        
+            si_1.SetXY(x_m[0]) 
 
-    coordinate_transform = CoordinateTransform(d=d, D=camera_coordinates_in_laser_coordinates[2], rotation_degree=mirror_rotation_deg)
+        end = time.time()
+        print("elapsed time: ", (end - start))    
+        cv2.putText(color_image, f"fps: {1 / (end - start)}", (10, 20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
-
-
-    y_m, x_m = coordinate_transform.target_to_mirror(camera_coordinates_in_laser_coordinates[1], camera_coordinates_in_laser_coordinates[0]) # order is changed in order to change x and y axis
-
-    
-    if(len(y_m) > 0 and len(x_m) > 0):
-        si_0.SetXY(y_m[0])        
-        si_1.SetXY(x_m[0]) 
-
-    end = time.time()
-    print("elapsed time: ", (end - start))    
-    cv2.putText(color_image, f"fps: {1 / (end - start)}", (10, 20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-
-    cv2.putText(color_image, f"Target Coordinates w.r.t. mirror center:", (10, 40), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"X: {camera_coordinates_in_laser_coordinates[0]}", (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"Y: {camera_coordinates_in_laser_coordinates[1]}", (10, 80), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.putText(color_image, f"Z: {camera_coordinates_in_laser_coordinates[2]}", (10, 100), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    color_image = cv2.circle(color_image, (int(new_circle.x), int(new_circle.y)), radius=10, color=(0, 255, 0), thickness=2)
-    # Show detected target position
-    cv2.imshow('Laser Detector',color_image)
-    # Press q key to stop
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.putText(color_image, f"Target Coordinates w.r.t. mirror center:", (10, 40), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"X: {camera_coordinates_in_laser_coordinates[0]}", (10, 60), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"Y: {camera_coordinates_in_laser_coordinates[1]}", (10, 80), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.putText(color_image, f"Z: {camera_coordinates_in_laser_coordinates[2]}", (10, 100), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+        color_image = cv2.circle(color_image, (int(new_circle.x), int(new_circle.y)), radius=10, color=(0, 255, 0), thickness=2)
+        # Show detected target position
+        cv2.imshow('Laser Detector',color_image)
+        # Press q key to stop
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 
-mre2.disconnect()
-print("done")
+    mre2.disconnect()
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
