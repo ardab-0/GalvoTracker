79846627e829a89ec9ea938e29bc1a69a9a725da
ardabuglagil <ardabuglagil@gmail.com>
2023-08-09 13:18:55 +0200

    update report

--- calibrate_camera_and_laser.py
+++ calibrate_camera_and_laser.py
@@ -10,6 +10,8 @@ import time
 from utils import optimal_rotation_and_translation
 import pickle
 from image_processing.black_white import black_and_white_threshold
+from image_processing.color_picker import Color_Picker
+
 
 # Constants 
 d = 0
@@ -19,7 +21,7 @@ save_path = "calibration_parameters"
 
 
 lower_red = np.array([140,   10, 240]) 
-upper_red = np.array([180, 130,256])
+upper_red = np.array([180, 130, 256])
 
 
 # x_t = np.array([0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100, 0, 25, 50, 75, 100])
@@ -75,6 +77,25 @@ ch_1.StaticInput.SetAsInput()                        # (1) here we tell the Mana
 ch_1.SetControlMode(optoMDC.Units.XY)           
 ch_1.Manager.CheckSignalFlow()                       # This is a useful method to make sure the signal flow is configured correctly.
 si_1 = mre2.Mirror.Channel_1.StaticInput
+
+# ret_color = False
+#  # Get capture
+# while not ret_color:
+#     capture = device.update()
+
+#     # Get the color image from the capture
+#     ret_color, color_image = capture.get_color_image()
+#     if not ret_color:
+#         continue
+
+#     cp = Color_Picker(color_image)
+#     color = cp.choose_pixel()
+#     hsv_color = cv2.cvtColor(np.array(color), cv2.COLOR_RGB2HSV)
+#     print(color)
+#     print(hsv_color)
+
+
+
 
 
 for z in z_t:
