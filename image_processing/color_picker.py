4eb5226525d94dbba72c6d75044bc0c64e5a010e
ardabuglagil <ardabuglagil@gmail.com>
2023-08-09 13:17:20 +0200

    convert color picker to class

--- image_processing/color_picker.py
+++ image_processing/color_picker.py
@@ -3,31 +3,48 @@ import numpy as np
 
 
 
+class Color_Picker():
 
-def click(event, x, y, flags, param):
-	
-	if event == cv2.EVENT_LBUTTONDOWN:
-		print(param[y, x])
-	
-
-i = 5
-
-image = np.load(f"image_processing/sphere_ims/depth_image_{i}.npy")
-image = image / np.max(image)
-
-image = cv2.imread(f"image_processing/sphere_ims/colored_depth_image_{i}.png")
+	def __init__(self, image):
+		self.image = image
+		self.color = None
 
 
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click, image)
+	def click(self, event, x, y, flags, param):
+		
+		if event == cv2.EVENT_LBUTTONDOWN:
+			print("Chosen color: ", self.image[y, x])
+			print("Press q to exit.")
+			self.color = self.image[y, x]
+		
 
-while True:
-	
-	cv2.imshow("image", image)
-	key = cv2.waitKey(1) & 0xFF	
-	
-	if key == ord("q"):
-		break
+	# i = 5
 
-# close all open windows
-cv2.destroyAllWindows()
+	# image = np.load(f"image_processing/sphere_ims/depth_image_{i}.npy")
+	# image = image / np.max(image)
+
+	# image = cv2.imread(f"image_processing/sphere_ims/colored_depth_image_{i}.png")
+
+	def choose_pixel(self):
+		cv2.namedWindow("image")
+		cv2.setMouseCallback("image", self.click)
+
+		while True:
+			
+			cv2.imshow("image", self.image)
+			key = cv2.waitKey(1) & 0xFF	
+			
+			if key == ord("q"):
+				break
+
+		# close all open windows
+		cv2.destroyAllWindows()
+		return self.color
+
+
+if __name__ == "__main__":
+	i = 5
+	image = cv2.imread(f"image_processing/sphere_ims/colored_depth_image_{i}.png")
+	cp = Color_Picker(image)
+
+	print(cp.choose_pixel())
